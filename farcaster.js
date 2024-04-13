00156184b18812e8063fd0a9448334b69446b3be
Red Panda <InfuraThrowaway@protonmail.com>
2024-04-13 13:40:20 -0400

    last minute changes to farcaster function

--- farcaster.js
+++ farcaster.js
@@ -3,23 +3,21 @@ const sdk = require('api')('@neynar/v2.0#79zo2aluds8jrx');
  
 function sendCasts(castArray){
     let sentArray = []
+    //organize by block height and remove duplicates
     castArray.sort((a, b) => a.blockHeight - b.blockHeight)
-    for(let cast of castArray){
-        if(sentArray.indexOf(cast.transactionHash) != -1 ){
+    for(let castObject of castArray){
+        if(sentArray.indexOf(castObject.transactionHash) != -1 ){
             continue;
         }
         console.log(cast)
-        sentArray.push(cast)
-
-    }
-    sdk.postCast({ text: string, signer_uuid: process.env.SIGNER_UUID }, { api_key: process.env.NEYNAR_API_KEY })
+        //sent casts out with a short delay 
+        setTimeout(sentArray.push(cast), 5000)
+        sdk.postCast({ text: castObject.cast, signer_uuid: process.env.SIGNER_UUID }, { api_key: process.env.NEYNAR_API_KEY })
         .then(({ data }) => console.log(data))
-        .catch(err => console.error("Error in API call:", err)); // Handle errors from the API call
-
+        .catch(err => console.error("Error in API call:", err)); // Handle errors from the API calL
+    }
     return sentArray
-
 }
-
 
 module.exports = { sendCasts };
 
